f5fc7203692eaa1bf22b357311f071b206f0e98b
athunlal <athunlalp@gmail.com>
2023-05-02 11:22:33 +0530

    completed

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"athunlal/testingg"
+)
+
+func main() {
+	testingg.Connect()
+}
---
+++ testingg/pgm_test.go
@@ -0,0 +1,28 @@
+package testingg_test
+
+import (
+	"testing"
+
+	"athunlal/testingg"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTask(t *testing.T) {
+	// Start mocking the database
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	// Define the expected insert query and result
+	expectedInsert := "INSERT INTO todo VALUES\\('test', '50'\\);?"
+
+	mock.ExpectExec(expectedInsert).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// Call the function being tested
+	testingg.CreateTask(db, "test", "50")
+
+	// Verify that the expected queries were executed
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
